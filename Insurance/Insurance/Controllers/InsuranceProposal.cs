9c8132853445f955d97daf438df9c8e82c07da1a
Lucio junior <luciojunin99@hotmail.com>
2025-09-03 21:39:14 -0300

    Commit - Criando a Solution

---
+++ Insurance/Insurance/Controllers/InsuranceProposal.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Insurance.Controllers
+{
+    public class InsuranceProposal : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
